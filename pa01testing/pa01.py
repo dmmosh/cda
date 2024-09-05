4b15250cbcc62e232a69f579e027477f716da9b2
wettestsock <evilsharkeater@gmail.com>
2024-09-05 13:58:32 -0400

    vds

--- pa01testing/pa01.py
+++ pa01testing/pa01.py
@@ -33,11 +33,11 @@ import os
 
 # converts the txt file to 2d list 
 #1st element (data[0][0]) is the matrix size (x by x)
-key = [list(map(int,line.split())) for line in open(argv[1])]
+key = [list(map(int,line.split())) for line in open(argv[1])][1:]
 
 
 plain = ''.join(ch for ch in open(argv[2]).read().lower() if ch.isalnum())  # the plaintext, lowercase and alpha only
-plain += str('x'*(key[0][0]-(len(plain)%key[0][0]))) # adds padding (key length - (plain length % key length ))
+plain += str('x'*(len(key)-(len(plain)%len(key)))) # adds padding (key length - (plain length % key length ))
 
 #sys.stdout.write(data) debug
 
@@ -50,9 +50,7 @@ sys.stdout.write('\t')
 sys.stdout.write('\t'.join(str(x) for x in open(argv[1]).readlines()[1:]))
 
 #sys.stdout.writes the plaintext
-sys.stdout.write('\nPlaintext:\n')
-
-
+sys.stdout.write('\nPlaintext:\n') # plaintext
 # 80 chars per sys.stdout.write line total
 i = 0 # inclusive, start of char sys.stdout.write
 j = 80 # exclusive, end of char sys.stdout.write
@@ -63,7 +61,8 @@ while(i < len(plain)): # while i is smaller than length of the plaintext
     i+=80 # increment i 
     j+=80 # increment j 
 
-sys.stdout.write('\nCiphertext:\n')
+sys.stdout.write('\nCiphertext:\n') # ciphertext portion
+# actually cool stuff
 
 
 #os.system('cat ' + argv[1] + ' | sed \'1d\'')
